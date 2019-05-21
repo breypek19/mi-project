00e30daed9384106bd6a3f564e717210adc0038c
brey <breynerpeke96@gmail.com>
2019-05-20 23:50:47 -0500

    agrego otro evento al index y al js

--- index.js
+++ index.js
@@ -5,9 +5,15 @@
 
 
 const ev=document.getElementById("unico");
+const ev1=document.getElementById("unico1");
 
 ev.addEventListener("click", function(){
 alert("hola");
 });
 
 
+
+ev1.addEventListener("mouseover", ()=>{
+
+    alert("paso el mouse");
+});
